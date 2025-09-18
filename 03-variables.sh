46f2fbcbe1cc72a847f3fb86496ce97efc28259d
Raja Shekar <devops86sraja@gmail.com>
2025-09-18 17:12:58 +0530

    update

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This is all about use of variables, first file
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 :: Hi $PERSON2, how are you?"
+echo "$PERSON2 :: Hi $PERSON1, I am fine, how are you?"
+echo "$PERSON1 :: I am good $PERSON2, what are you doing these days"
+echo "$PERSON2 :: nothing just regular work as usual $PERSON1"
