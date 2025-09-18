7e2e35ce2344202fd71e3720d3c7df63bb8fa57b
Raja Shekar <devops86sraja@gmail.com>
2025-09-18 17:08:37 +0530

    newfiles

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Print Conversation between two persons
+
+echo "Ramesh :: Hi Suresh, how are you?"
+echo "Suresh :: Hi Ramesh, I am fine, how are you?"
+echo "Ramesh :: I am good Suresh, what are you doing these days"
+echo "Suresh :: nothing just regular work as usual Ramesh"
