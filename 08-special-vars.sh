3bce9141da679fb87bd8e86663e96ecdd7334d37
Raja Shekar <devops86sraja@gmail.com>
2025-09-20 08:56:33 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,12 @@
 echo "All variables passed to this script: $@"
 echo "All variables passed to this script: $*"
 
+echo "Script name: $0"
+echo "Current directory: $PWD"
+
+echo "Who is running this: $USER"
+echo "Home directory is: $HOME"
+echo "PID of this script: $$"
+
+sleep 50&
+echo "PID of last command in background is: $!"
