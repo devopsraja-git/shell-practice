72d312dbd38a717f3db53b0be49ea2bd3ed0dae9
Raja Shekar <devops86sraja@gmail.com>
2025-09-20 07:58:04 +0530

    new update

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Arguments:
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "PERSON1:: Welcome to USA PERSON2"
+echo "PERSON2:: Thank you! finally not on zoom PERSON1"
+echo "PERSON1:: So, do we have a deal?"
+echo "PERSON2:: Yes, if you make Ukraine join Russia"
