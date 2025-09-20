b0eed3a7eb7149771096a9749ea68fd8585eda53
Raja Shekar <devops86sraja@gmail.com>
2025-09-20 09:33:09 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -11,4 +11,9 @@ echo "Sum of 2 numbers is: ${SUM}"
 
 LEADERS=("NTR" "CBN" "YSR" "KCR")
 
+# Size = 4, Max index = 3
+echo "First Leader is: ${LEADERS[0]}"
+echo "Second Leader is: ${LEADERS[1]}"
+echo "Third Leader is: ${LEADERS[2]}"
+echo "Fourth Leaders is: ${LEADERS[3]}"
 echo "All leaders: ${LEADERS[@]}"
