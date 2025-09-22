e6e3bdc6bdaf9694eff8f3396de04d383bbc0e83
Raja Shekar <devops86sraja@gmail.com>
2025-09-22 14:05:53 +0530

    conditions

--- 12-conditions-primenums.sh
+++ 12-conditions-primenums.sh
@@ -5,7 +5,7 @@
 echo "Please enter the number: "
 read NUMBER
 
-if [ $($($(NUMBER) % $(NUMBER))) -eq 0 -gt 1 ]; then
+if [ $($(NUMBER) % $(NUMBER)) -eq 0 && -gt 1 ]; then
     echo "Given number  $NUMBER is a PRIME Number"
 else
     echo "Given number $NUMBER is not a PRIME Number"
