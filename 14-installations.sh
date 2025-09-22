1d8094a4071532f71865bf0f811e5259c73e6485
Raja Shekar <devops86sraja@gmail.com>
2025-09-22 15:33:42 +0530

    conditions

---
+++ 14-installations.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Installations of mySQL, Nginx, python3
+
+#user
+
+uid=$(id -u)
+
+if [ $uid -ne 0 ]; then
+    echo "ERROR:: Please run this script with ROOT Privileges"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installation of $2 is failed"
+        exit 1
+    else
+        echo "Installation of $2 is SUCCESSFUL"
+        exit 0
+    fi
+}
+
+dnf install mysql -y
+validate $? "mySQL"
+
+dnf install nginx -y
+validate $? "Nginx"
+
+dnf install python -y
+validate $? "python"
