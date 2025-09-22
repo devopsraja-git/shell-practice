4c308589fce87efead4a68057abcc0990eed0e5b
Raja Shekar <devops86sraja@gmail.com>
2025-09-22 18:07:22 +0530

    conditions

--- 18-logs.sh
+++ 18-logs.sh
@@ -26,10 +26,10 @@ fi
 
 validate(){
     if [ $1 -ne 0 ]; then
-    echo -e "ERROR:: Installation of $2 is $R FAILED $N" &>> $LOG_FILE
+    echo -e "ERROR:: Installation of $2 is $R FAILED $N"
     exit 1
 else
-    echo -e "Installation of $2 is $G SUCCESSFUL $N" &>> $LOG_FILE
+    echo -e "Installation of $2 is $G SUCCESSFUL $N"
 fi
 }
 
