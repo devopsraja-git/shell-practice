5a20738f1699fa39940d89ef6de2df5a49c2269a
Raja Shekar <devops86sraja@gmail.com>
2025-09-22 14:10:41 +0530

    conditions

--- 12-conditions-primenums.sh
+++ 12-conditions-primenums.sh
@@ -7,6 +7,6 @@ read NUMBER
 
 if [ $NUMBER -lt 1 ]; then
     echo "Given number  $NUMBER is not a PRIME Number"
-elif [ $($NUMBER % $NUMBER) -eq 0 ]; then
+elif [ $($(NUMBER) % $(NUMBER)) -eq 0 ]; then
     echo "Given number $NUMBER is a PRIME Number"
 fi
