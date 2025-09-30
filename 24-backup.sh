ed81251c7bf72d7e3a29d492f0980137cec771ca
Raja Shekar <devops86sraja@gmail.com>
2025-09-30 10:50:54 +0530

    new updates

--- 24-backup.sh
+++ 24-backup.sh
@@ -31,26 +31,42 @@ USAGE(){
     if [ $# -lt 2 ]; then
         USAGE
     fi
-
+#check source directory exists or not
         if [ ! -d $SOURCE_DIR ]; then
             echo -e "$R Source $SOURCE_DIR doesnot exist $N"
             exit 1
         fi
-
+#check destination directory exists or not
         if [ ! -d $DEST_DIR ]; then
             echo -e "$R Destination $DEST_DIR doesnot exist $N"
             exit 1
         fi
 
+#find the files to be archived and deleted
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 
 if [ ! -z "${FILES}" ]; then
-    echo "Files found $FILES"
+    echo "Files found: $FILES" #Start arvhiving the files
     TIME_STAMP=$(date +%F)
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIME_STAMP.zip"
     echo "Zip file name: $ZIP_FILE_NAME"
-    echo "$FILES" | zip  -@ -j "$ZIP_FILE_NAME"
+    echo "$FILES" | zip  -@ -j "$ZIP_FILE_NAME" #give -j for not storing with full path"
+
+    if [ -f $ZIP_FILE_NAME ]; then
+        echo -e "Archival...$G SUCCESS $N"
+
+        while IFS= read -r filepath
+        do
+        echo "Deleting the file: $filepath"
+        rm -rf $filepath
+        echo "Deleted the file: $filepath"
+        done <<< $FILES
+    else
+        echo -e "Archival...$R FAILED $N"
+        exit 1
+    fi
+
 else
     echo -e "No Files to archive  $Y SKIPPING.. $N"
 fi
