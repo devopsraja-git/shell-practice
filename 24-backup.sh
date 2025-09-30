462a8f36fade7c35973d9def6efba1d48463b721
Raja Shekar <devops86sraja@gmail.com>
2025-09-30 10:38:34 +0530

    new updates

--- 24-backup.sh
+++ 24-backup.sh
@@ -42,7 +42,7 @@ USAGE(){
             exit 1
         fi
 
-FILES=$(find $SOURCE_DIR -name '*.log' -type f -mtime +$DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 
 if [ ! -z "${FILES}" ]; then
